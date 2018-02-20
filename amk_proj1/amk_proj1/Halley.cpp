18ce8d747ed4764d8cf1eef3cb7123882fd2f7aa
pupdum <cpt.armadillo@gmail.com>
2018-02-20 10:37:02 -0500

    gorsh, goofy

--- amk_proj1/amk_proj1/Halley.cpp
+++ amk_proj1/amk_proj1/Halley.cpp
@@ -9,11 +9,11 @@
 //Group of recursive Halley functions
 
 double HalleyRecursiveA(double guess) {
-	double suffering = ((2 * f1(guess)* f1_prime(guess)) / (2 * (pow(f1_prime(guess), 2) - f1(guess)* f1_dprime(guess))));
-	double x_n = guess - suffering;
+	double suffering = ((2 * f1(guess)* f1_prime(guess)) / (2 * (pow(f1_prime(guess), 2) - f1(guess)* f1_dprime(guess)))); //The term that gets subtracted, separated to make code more legible
+	double x_n = guess - suffering; //Subtract that stuff from initial value
 
 	if (suffering > 0.0001) {
-		return HalleyRecursiveA(x_n);
+		return HalleyRecursiveA(x_n); //ideally causes recursion, stopping when the change is less than the specified amount
 	}
 	else {
 		return x_n;
@@ -21,7 +21,7 @@ double HalleyRecursiveA(double guess) {
 }
 
 double HalleyRecursiveB(double guess) {
-	double pain = ((2 * f2(guess)* f2_prime(guess)) / (2 * (pow(f2_prime(guess), 2) - f2(guess)* f2_dprime(guess))));
+	double pain = ((2 * f2(guess)* f2_prime(guess)) / (2 * (pow(f2_prime(guess), 2) - f2(guess)* f2_dprime(guess)))); //Same as above except uses the second function
 	double x_n = guess - pain;
 
 	if (pain > 0.0001) {
@@ -32,7 +32,7 @@ double HalleyRecursiveB(double guess) {
 	}
 }
 
-//not sure what the recursiveC and iterativeC functions are for, considering I'll only need four functions to implement two versions of Halley's method I think.
+
 //double HalleyRecursiveC(double guess) {
 
 
@@ -40,11 +40,31 @@ double HalleyRecursiveB(double guess) {
 //Group of iterative Halley functions
 
 double HalleyIterativeA(double guess) {
-
+	double temp_var = guess; //This makes it easier to change the value for the next step
+	for (int i = 0; i < 100; i++) {
+		double foo = ((2 * f1(temp_var)* f1_prime(temp_var)) / (2 * (pow(f1_prime(temp_var), 2) - f1(temp_var)* f1_dprime(temp_var)))); //evaluates expression
+		if (foo > 0.0001) {
+			temp_var -= foo; //When change is >0.0001 it will keep going
+		}
+		else {
+			return temp_var; //when change is < 0.0001 it will return the last result and exit.
+			break;
+		}
+	}
 }
 
 double HalleyIterativeB(double guess) {
-
+	double temp_var = guess; //This makes it easier to change the value for the next step
+	for (int i = 0; i < 100; i++) {
+		double foo = ((2 * f2(temp_var)* f2_prime(temp_var)) / (2 * (pow(f2_prime(temp_var), 2) - f2(temp_var)* f2_dprime(temp_var)))); //evaluates expression
+		if (foo > 0.0001) {
+			temp_var -= foo; //When change is >0.0001 it will keep going
+		}
+		else {
+			return temp_var; //when change is < 0.0001 it will return the last result and exit.
+			break;
+		}
+	}
 }
 
 //double HalleyIterativeC(double guess) {
